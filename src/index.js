78c03147798327a303b6ef54134e28a547a0fb0e
Justin Mason <justinmason@Justins-MacBook-Pro.local>
2018-07-21 18:36:17 -0400

    Add queue functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const activities_reader = require(__dirname + "/file_handling/activities_reader.
 const resources_reader = require(__dirname + "/file_handling/resources_reader.js");
 const tasks_reader = require(__dirname + "/file_handling/tasks_reader.js");
 const Logger = require(__dirname + "/logger/logger.js");
+const Queue = require(__dirname + "/task_handling/queue.js");
 
 const commandLineConfig = projectRunConfig.getConfig(process.argv); //eslint-disable-line
 
@@ -13,5 +14,7 @@ const resources = resources_reader.getResources(commandLineConfig);
 
 let tasks = tasks_reader.getTasks(commandLineConfig);
 tasks = activities_reader.addActivitiesToTasks({commandLineConfig, tasks});
+
+const queue = new Queue(tasks);
 
 console.log();
---
+++ src/task_handling/queue.js
@@ -0,0 +1,28 @@
+module.exports =
+
+class Queue {
+
+	constructor(tasks) {
+		this.queue = tasks ? Object.keys(tasks) : [];
+	}
+
+	add(taskID) {
+		this.queue.push(taskID);
+	}
+
+	remove() {
+		const taskID = this.queue[0];
+		this.queue.shift();
+		return taskID;
+	}
+
+	isEmpty() {
+		return(this.queue.length == 0);
+	}
+
+	toString() {
+		console.log(typeof this.queue);
+		console.log("" + this.queue);
+	}
+
+};
