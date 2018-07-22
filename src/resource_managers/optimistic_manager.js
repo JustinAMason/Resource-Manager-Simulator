9136e08f5fcaa5cbf599700346c2b692c696f805
Justin Mason <justinmason@Justins-MacBook-Pro.local>
2018-07-21 21:30:48 -0400

    Add additional functionality to optimistic_manager

--- src/resource_managers/optimistic_manager.js
+++ src/resource_managers/optimistic_manager.js
@@ -11,30 +11,85 @@ class OptimisticManager extends ResourceManager {
 	run() {
 
 		let curCycle = 1;
-		this.logger.logHeader("Optimistic Reesource Manager Simulation");
+
+		this.logger.logHeader("Optimistic Resource Manager Simulation");
 
 		while (!this.queue.isEmpty()) {
+
 			this.showResourcesAvailable();
-			this.runCycle();
-			break;
+
+			for (let i = 0; i < this.queue.size(); i++) {
+				this.runCycle();
+			}
+
+			console.log();
+
 		}
+
+		this.logger.logHeaderBreak();
 
 	}
 
 	runCycle() {
-		console.log("Running cycle...");
+
+		const taskID = this.queue.remove();
+		const task = this.tasks[taskID];
+		const activities = task["activities"];
+		const activity = activities[0];
+		const action = activity["action"];
+
+		/* eslint-disable indent */
+		switch (action) {
+			case "request": this.request(); break;
+			case "release": this.release(); break;
+			case "terminate": this.terminate(); break;
+			default: break;
+		}
+
+		activities.shift();
+
+		this.updateTasks(taskID);
+
+	}
+
+	updateTasks(taskID) {
+
+		const task = this.tasks[taskID];
+		const activities = task["activities"];
+
+		if (activities.length == 0) {
+			delete this.tasks[taskID];
+		} else {
+			this.queue.add(taskID);
+		}
+
+	}
+
+	request() {
+		console.log("REQUEST");
+	}
+
+	release() {
+		console.log("RELEASE");
+	}
+
+	terminate() {
+		console.log("TERMINATE");
 	}
 
 	showResourcesAvailable() {
+
 		const resources = this.resources;
+
 		let output = "AVAILABLE: ";
 		Object.keys(this.resources).forEach(function(resourceID) {
 			output += output === "AVAILABLE: " ? "": ", ";
 			output += `R${resourceID} (${resources[resourceID]})`;
 		});
+
 		this.logger.log(output);
 		this.logger.logLine();
-		return;
+		
 	}
 
 };
