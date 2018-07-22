7bae7d72f1bef9cf60bee77ecc3eb1801a3cf91a
Justin Mason <justinmason@Justins-MacBook-Pro.local>
2018-07-21 20:41:42 -0400

    Add additional functionality for Logger

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -6,11 +6,36 @@ class Logger {
 
 	constructor(args) {
 		this.detailedOutput = args["detailed_output"] ? args["detailed_output"] : false;
+		this.lineLength = args["line_length"] ? args["line_length"] : 70;
 	}
 
 	log(output) {
 		if (this.detailedOutput) {
 			console.log(output);
+		}
+	}
+
+	logHeader(header) {
+		if (this.detailedOutput) {
+			const line = "=".repeat((this.lineLength - header.length) / 2);
+			let output = line + header.toUpperCase() + line;
+			if (output.length < 70) {
+				output += "=";
+			}
+			console.log(output);
+			console.log();
+		}
+	}
+
+	logHeaderBreak() {
+		if (this.detailedOutput) {
+			console.log("=".repeat(this.lineLength));
+		}
+	}
+
+	logLine() {
+		if (this.detailedOutput) {
+			console.log("-".repeat(this.lineLength / 2));
 		}
 	}
 
