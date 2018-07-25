97c070a641f3e419b045265fb3fd7a7e21e4f2e8
Justin Mason <justinmason@Justins-MacBook-Pro.local>
2018-07-24 23:39:36 -0400

    Remove all references to tape

--- test/resource_managers/test_resource_manager.js
+++
@@ -1,6 +0,0 @@
-const test = require("tape"); // assign the tape library to the variable "test"
-
-// test("RESOURCE MANAGER", function (t) {
-// 	t.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so passes
-// 	t.end();
-// });
