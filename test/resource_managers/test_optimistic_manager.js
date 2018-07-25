587906e2eb880e1e0700f247e107c6e37fc71d8f
Justin Mason <justinmason@Justins-MacBook-Pro.local>
2018-07-24 22:50:41 -0400

    Refactor test names

---
+++ test/resource_managers/test_optimistic_manager.js
@@ -0,0 +1,6 @@
+const test = require("tape"); // assign the tape library to the variable "test"
+
+// test("OPTIMISTIC MANAGER", function (t) {
+// 	t.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so passes
+// 	t.end();
+// });
---
+++ test/resource_managers/test_resource_manager.js
@@ -0,0 +1,6 @@
+const test = require("tape"); // assign the tape library to the variable "test"
+
+// test("RESOURCE MANAGER", function (t) {
+// 	t.equal(-1, [1,2,3].indexOf(4)); // 4 is not present in this array so passes
+// 	t.end();
+// });
