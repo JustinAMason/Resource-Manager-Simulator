65ae24eb2eea59f9783a9ab26cf45befa27bad35
Justin Mason <justinmason@Justins-MacBook-Pro.local>
2018-07-24 23:51:45 -0400

    Refactors file_handling/tasks_reader.getTasks() tests

--- test/file_handling/test_tasks_reader.js
+++ test/file_handling/test_tasks_reader.js
@@ -8,14 +8,17 @@ describe("tasks_reader", function() {
 
 	describe("#getTasks()", function() {
 
-		context("Valid file path given", function() {
+		context("Valid file paths given", function() {
 
-			const commandLineConfig = { file: "test/test_input_files/input-01.txt" };
-			const expected = factories.input_01_tasks_without_activities;
-			const actual = tasks_reader.getTasks(commandLineConfig);
+			const commandLineConfigs = factories.command_line_configs;
+			const expectations = factories.tasks_without_activities;
 
-			it("should create a tasks object without activities from the file path given", function() {
-				expect(expected).to.deep.equal(actual);
+			it("should create tasks objects without activities from the file paths given", function() {
+				commandLineConfigs.forEach(function(commandLineConfig, i) {
+					const expected = expectations[i];
+					const actual = tasks_reader.getTasks(commandLineConfig);
+					expect(expected).to.deep.equal(actual);
+				});
 			});
 
 		});
@@ -26,7 +29,7 @@ describe("tasks_reader", function() {
 			const expected = factories.input_01_tasks_without_activities;
 			const actual = tasks_reader.getTasks(commandLineConfig);
 
-			it("should create a tasks object without activities from the file path given", function() {
+			it("should create a tasks object without activities from the default file path (input_01)", function() {
 				expect(expected).to.deep.equal(actual);
 			});
 
