98eafcc8443a6be45c0c834a2a44a50832061f79
Justin Mason <justinmason@Justins-MacBook-Pro.local>
2018-07-26 18:28:17 -0400

    Refactors test descriptions in project_run_config.test

--- test/config/project_run_config.test.js
+++ test/config/project_run_config.test.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 describe("project_run_config", function() {
 	describe("#getConfig()", function() {
 		context("second command line argument given", function() {
-			it("converts arguments into an object; output version set to detailed", function() {
+			it("converts command line arguments into a command line configuration object; output version set to detailed", function() {
 				const args = [ "command", "run_file", "input_file", "argument"];
 				const expected = {"file": "input_file", "show_output": true};
 				const actual = project_run_config.getConfig(args, "test");
@@ -14,7 +14,7 @@ describe("project_run_config", function() {
 			});
 		});
 		context("second command line argument not given", function() {
-			it("converts arguments into an object; output version set to results only", function() {
+			it("converts command line arguments into a command line configuration object; output version set to results only", function() {
 				const args = [ "command", "run_file", "input_file", "argument"];
 				const expected = {"file": "input_file", "show_output": true};
 				const actual = project_run_config.getConfig(args, "test");
