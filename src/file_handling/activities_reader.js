4d86af83023432863c8f6246f48ddff99f6a7689
Justin Mason <justinmason@Justins-MacBook-Pro.local>
2018-07-27 21:58:25 -0400

    Refactor activities_reader.js

--- src/file_handling/activities_reader.js
+++ src/file_handling/activities_reader.js
@@ -5,74 +5,72 @@ module.exports = {
 };
 
 function addActivitiesToTasks(args) {
-	const file = args["commandLineConfig"]["file"];
-	let tasks = args["tasks"];
-	const data = file_reader.readFile(file);
-	const activitiesData = getActivitiesData(data.split("\n"));
-	const activities = getActivities(activitiesData);
-	tasks = addActivities({tasks, activities});
-	return(tasks);
-}
-
-// private method
-function getActivitiesData(data) {
-
-	data.shift();
-
-	data = data.filter(function(activity) {
-		const activityType = activity.split(" ")[0];
-		return activityType !== "";
-	});
-
-	return(data);
+	const tasks = args["tasks"];
+	const data = file_reader.readFile(args["commandLineConfig"]["file"]);
+	const activities = getActivities(getActivitiesData(data.split("\n")));
+	return addActivities(tasks, activities);
 }
 
 // private method
 function getActivities(activitiesLog) {
-
 	const activities = [];
-
 	let activityID = 1;
 
-	activitiesLog.forEach(function(activityData) {
-		activityData = activityData.split(" ");
-		activityData = activityData.filter(function(x) {
-			return(x !== "");
-		});
-
-		const action = activityData[0];
-		const taskID = activityData[1];
-		const delay = activityData[2];
-
-		const resourceID = activityData[3];
-		const quantity = activityData[4];
-
-		activities.push({
-			"activityID": activityID,
-			"action": action,
-			"taskID": taskID,
-			"delay": delay,
-			"resourceID": resourceID,
-			"quantity": quantity
-		});
-
+	activitiesLog.forEach(function(activityString) {
+		activities.push(getActivity(activityID, getActivityParts(activityString)));
 		activityID += 1;
-
 	});
 
 	return(activities);
 }
 
-// private method
-function addActivities(args) {
-	const tasks = args["tasks"];
-	const activities = args["activities"];
+//private method
+function getActivity(activityID, activityParts) {
+	const [action, taskID, delay, resourceID, quantity] = activityParts;
+	return {
+		"activityID": activityID,
+		"action": action,
+		"taskID": taskID,
+		"delay": delay,
+		"resourceID": resourceID,
+		"quantity": quantity
+	};
+}
 
+// private method
+function getActivityParts(activityString) {
+	return activityString.split(" ").filter(function(activityPart) {
+		return(activityPart !== "");
+	});
+}
+
+// private method
+function getActivitiesData(data) {
+	return data.filter(function(activity, index) {
+		return isValidActivity(getActivityType(activity), index);
+	});
+}
+
+//private method
+function isValidActivity(activityType, activityIndex) {
+	return activityType !== "" && activityIndex > 0;
+}
+
+// private method
+function getActivityType(activity) {
+	return activity.split(" ")[0];
+}
+
+//private method
+function addActivities(tasks, activities) {
 	activities.forEach(function(activity) {
-		const taskID = activity["taskID"];
-		tasks[taskID]["activities"].push(activity);
+		addActivity(tasks, activity);
 	});
 
 	return(tasks);
+}
 
+//private method
+function addActivity(tasks, activity) {
+	tasks[activity["taskID"]]["activities"].push(activity);
 }
